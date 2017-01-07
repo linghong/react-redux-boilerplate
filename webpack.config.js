e11bebeb17bd904491d1e8e7d2d21db8021a472e
linghong <linghong_chen@hotmail.com>
2017-01-07 14:30:36 -0500

    set up basic webpack structure.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path=require('path');
+
+module.exports={
+	entry: './src/index.js',
+	output: {
+		//must specify an absolouate path, __dirname references to current directory
+		//save the name as "build"
+		"path": path.resolve(__dirname, 'build'),
+		"filename": 'bundle.js'
+	}
+}
