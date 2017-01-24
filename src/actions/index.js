a2a19906bcab655d44e1bd15a0151f76f1629e5b
linghong <linghong_chen@hotmail.com>
2017-01-24 10:02:10 -0500

    added action to fetch data through restful API using axios.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,20 @@
 //An action is a plain js object and must have a type
+import axios from 'axios';
+
+export const FETCH_DATA = 'FETCH_DATA';
+
+const ROOT_URL = "https://react-redux-40894.firebaseio.com";
+const API="examples";
+
+export function fetchData(){
+	const request =axios.get(`${ROOT_URL}/${API}.json`);
+	console.log(request);
+	return {
+		type: FETCH_DATA,
+		payload: request
+	};
+}
+
 export function selectItem(item){
 	//selectImage is an ActionCreater, 
 	//An action creater is a js function which returns an action.
