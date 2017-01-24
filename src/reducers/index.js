637b6c9bbbe2b58bfdc570b0b6be025c91492f6e
linghong <linghong_chen@hotmail.com>
2017-01-24 10:06:25 -0500

    added FETCH_DATA reducer for the item list.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 //functions that return application states
 import { combineReducers } from 'redux';
-import Items from './reducer_items';
-import ActiveItem from './reducer_active_item';
+import ItemsReducer from './reducer_items';
+import ActiveItemReducer from './reducer_active_item';
 
 const rootReducer = combineReducers({
-	items: Items,
-	activeItem: ActiveItem
+	items: ItemsReducer,
+	activeItem: ActiveItemReducer
 });
 
 export default rootReducer;
